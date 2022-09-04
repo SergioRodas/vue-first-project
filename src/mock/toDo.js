30429c654f23f2c2deb35e039e296993778438c1
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-09-03 21:23:39 -0300

    Adds static data

---
+++ src/mock/toDo.js
@@ -0,0 +1,19 @@
+const toDo = [
+  {
+    id: 1,
+    name: "Aprender Ángular",
+    completed: false
+  },
+  {
+    id: 2,
+    name: "Armar crud con Vue",
+    completed: false
+  },
+  {
+    id: 3,
+    name: "Crear proyecto con funcionalidad cliente-servidor en tiempo real, y con procesamientos en segundo plano en el backend que a su vez se notifiquen en el front.",
+    completed: false
+  }
+];
+
+export default toDo;
