d66312b0d8d4d9f6a6636bd9b98a659ba7bc52bb
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-09-08 21:30:17 -0300

    Modify the main component structure

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-  <RouterView />
+  <div class="container">
+    <ul>
+      <RouterLink to="/"><li>Todo List</li></RouterLink>
+      <!-- <RouterLink to="/new"><li>New Todo</li></RouterLink> -->
+    </ul>
+    <RouterView />
+  </div>
 </template>
 
 <style>
