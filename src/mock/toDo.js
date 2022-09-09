9d45d0401762bcc6eab64eb5b5be026313715d53
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-09-08 21:32:20 -0300

    Adds a new task

--- src/mock/toDo.js
+++ src/mock/toDo.js
@@ -13,6 +13,11 @@ const toDo = [
     id: 3,
     name: "Crear proyecto con funcionalidad cliente-servidor en tiempo real, y con procesamientos en segundo plano en el backend que a su vez se notifiquen en el front.",
     completed: false
+  },
+  {
+    id: 4,
+    name: "Repasar React.",
+    completed: true
   }
 ];
 
